b01f16492f98f72153a075f952559775ab8b1a37
Imris <imris01@163.com>
2024-07-23 14:35:37 +0800

    Add files via upload

---
+++ Reversi/code/player.h
@@ -0,0 +1,19 @@
+/**
+ * @file player.h
+ * @author yangboyang@jisuanke.com
+ * @copyright jisuanke.com
+ * @date 2021-07-01
+ */
+
+#include <string.h>
+#include "../include/playerbase.h"
+#include <stdio.h>
+
+void init(struct Player *player) {
+	// This function will be executed at the begin of each game, only once.
+}
+
+struct Point place(struct Player *player) {
+
+	return initPoint(-1, -1);   // give up
+}
